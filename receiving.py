1b945c18153f7304416f5896e26543c39a684a13
nova_sangeeth <novasangeeth@outlook.com>
2021-07-22 00:40:06 +0530

    changes to the callback function to accomodate postgres backup methods.

--- receiving.py
+++ receiving.py
@@ -1,25 +1,28 @@
 import pika, sys, os
+from db_conn import connection_test
+from db_backup_methods import  backup_postgres_db
+import datetime
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+channel = connection.channel()
+channel.queue_declare(queue="hello")
 
-def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
+time = datetime.datetime.now()
 
-    channel.queue_declare(queue='hello')
 
-    def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
+def callback(ch, method, properties, body):
+    # connection_test()
+    backup_postgres_db(
+        database_name="app",
+        dest_file=f'test_{time}.sql', 
+        host='localhost', 
+        password='postgres', 
+        port=5432, 
+        user= 'postgres',
+        table_only=True
+    )
+    print(" [x] Received %r" % body)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
-
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+channel.basic_consume(queue="hello", on_message_callback=callback, auto_ack=True)
+print(" [*] Waiting for trigger.")
+channel.start_consuming()
