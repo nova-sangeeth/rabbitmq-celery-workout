5cb560ed40ef07f61b61d9a7eb457b17c0a247f4
nova_sangeeth <novasangeeth@outlook.com>
2021-07-22 00:41:46 +0530

    minor changes to the send

--- send.py
+++ send.py
@@ -6,6 +6,6 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello  World!')
-print(" [x] Sent 'Hello World!'")
+channel.basic_publish(exchange='', routing_key='hello', body='DATABASE CHECK')
+print("Sending Message -->> 'CHECKING DB CONNECTION!'")
 connection.close()
