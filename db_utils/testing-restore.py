d141a7fad333db523f7131ffdd53840f8bbbdd69
nova_sangeeth <novasangeeth@outlook.com>
2021-08-05 14:31:17 +0530

    new command that needs to be executed in pgdump or psql.

--- db_utils/testing-restore.py
+++ db_utils/testing-restore.py
@@ -10,77 +10,69 @@ logger = logging.getLogger(__name__)
 
 import os
 
-download_link = 'https://talentfinddev.blob.core.windows.net/referencedata/data/data_backupreference_data_backup/ref_data_2021_08_01_19_09_23.sql'
-print('file path for the reference data backup: ' + download_link)
-r = requests.get(download_link, stream=True)
-with open("dump.sql",'wb') as sql_dump_file:
-    for chunk in r.iter_content(chunk_size=2048): # 2 mb chucks
-        if chunk: 
-            sql_dump_file.write(chunk)
-backup_file_path = os.path.realpath(sql_dump_file.name)
-print('file path' + backup_file_path)
-
-# def restore_postgres_db(db_host, db, port, user, password, backup_file, verbose):
-#     """
-#     Restore postgres db from a file.
-#     """
-
-#     if verbose:
-#         try:
-#             print(user,password,db_host,port, db)
-#             process = subprocess.Popen(
-#                 [
-#                     'pg_restore',
-#                     '--verbose',
-#                     '--clean',
-#                     # '--data-only',
-#                     '--dbname=postgresql://{}:{}@{}:{}/{}'.format(user, password, db_host, port, db),
-#                     backup_file
-#                 ],
-#                 stdout=subprocess.PIPE
-#             )
-#             output = process.communicate()[0]
-#             if int(process.returncode) != 0:
-#                 print('Command failed. Return code : {}'.format(process.returncode))
-
-#             return output
-#         except Exception as e:
-#             print("Issue with the db restore : {}".format(e))
-#     else:
-#         try:
-#             process = subprocess.Popen(
-#                 [
-#                     'pg_restore', 
-#                     '--verbose', 
-#                     '--clean', 
-#                     '--dbname=postgresql://{}:{}@{}:{}/{}'.format(user,password,db_host,port, db),
-#                     backup_file
-#                 ],
-#                 stdout=subprocess.PIPE
-#             )
-#             output = process.communicate()[0]
-#             if int(process.returncode) != 0:
-#                 print('Command failed. Return code : {}'.format(process.returncode))
-
-#             return output
-#         except Exception as e:
-#             print("Issue with the db restore : {}".format(e))
 
 
-# def restore_reference_data():
-#     try:
-#         restore_postgres_db(
-#             db_host="localhost",
-#             db="app",
-#             port=5432,
-#             user="postgres",
-#             password="postgres",
-#             backup_file="./database-new.sql",
-#             verbose=True
-#         )
-#     except Exception as e:
-#         logger.warning("Issue with the db restore : {}".format(e))
-#     return "DB restore done!!"
+def restore_postgres_db(db_host, db, port, user, password, backup_file):
+    try:
+        print(user,password,db_host,port, db)
+        process = subprocess.Popen(
+            [
+                'pg_restore',
+                '--verbose',
+                '--clean',
+                '--dbname=postgresql://{}:{}@{}:{}/{}'.format(user, password, db_host, port, db),
+                backup_file
+            ],
+            stdout=subprocess.PIPE
+        )
+        output = process.communicate()[0]
+        if int(process.returncode) != 0:
+            print('Command failed. Return code : {}'.format(process.returncode))
+
+        return output
+    except Exception as e:
+        print("Issue with the db restore : {}".format(e))
 
 
-# restore_reference_data()
+def restore_reference_data():
+    try:
+        backup_file_path = "dump.sql"
+        logger.debug('Temporary file path ==>' + backup_file_path)
+    
+    except Exception as e:
+        logger.warning("Issue with retreiving the link : {}".format(e))
+    
+    try:
+        restore_postgres_db(
+            db_host = 'db',
+            db = 'app', 
+            port = 5432, 
+            user = 'postgres', 
+            password = 'postgres', 
+            backup_file = backup_file_path
+        )
+        os.remove(backup_file_path)
+        print('removing temp file.....')
+        return 'DB restore done!!'
+
+    except Exception as e:
+            logger.warning("Issue with the db restore : {}".format(e))
+            return 'DB restore failed!!'
+
+
+def concat_sqlfiles(file_path):
+    try:
+        process = subprocess.Popen(
+            [
+                        
+            ],
+            stdout=subprocess.PIPE
+        )
+        output = process.communicate()[0]
+        if int(process.returncode) != 0:
+            print('Command failed. Return code : {}'.format(process.returncode))
+
+        print('Successfully Completed')
+    except:
+        print('Error Unable to concat')
+    return None
