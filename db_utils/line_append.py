a66e56ec33ce4aaf07514152c42ca6fbe0e3c32c
nova_sangeeth <novasangeeth@outlook.com>
2021-08-09 16:14:17 +0530

    changes to add a new line at the top.

---
+++ db_utils/line_append.py
@@ -0,0 +1,11 @@
+import os
+
+def insert(originalfile,string):
+    with open(originalfile,'r') as f:
+        with open('newfile.sql','w') as f2: 
+            f2.write(string)
+            f2.write(f.read())
+    os.rename('newfile.sql',originalfile)
+
+
+insert('ref_data_2.sql', 'SET session_replication_role to replica;')
