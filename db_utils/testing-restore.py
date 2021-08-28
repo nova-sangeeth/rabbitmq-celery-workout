696c36f18bab8f2d42609f38986ce33a25ba6c86
nova_sangeeth <novasangeeth@outlook.com>
2021-08-28 13:46:23 +0530

    changes to the restore script.

--- db_utils/testing-restore.py
+++ db_utils/testing-restore.py
@@ -11,17 +11,18 @@ logger = logging.getLogger(__name__)
 import os
 
 
-
 def restore_postgres_db(db_host, db, port, user, password, backup_file):
     try:
         print(user,password,db_host,port, db)
         process = subprocess.Popen(
             [
-                'psql',
-                '-c',
+                'pg_restore',
+                '--no-owner',
                 '--dbname=postgresql://{}:{}@{}:{}/{}'.format(user, password, db_host, port, db),
+                '-v',
                 backup_file
             ],
+            env={'PGPASSWORD': 'postgres'},
             stdout=subprocess.PIPE
         )
         output = process.communicate()[0]
@@ -35,7 +36,7 @@ def restore_postgres_db(db_host, db, port, user, password, backup_file):
 
 def restore_reference_data():
     try:
-        backup_file_path = "reference_data_1.sql"
+        backup_file_path = "/home/novasangeeth/Code--dev/rabbit-mq/ref_data.sql"
         logger.debug('Temporary file path ==>' + backup_file_path)
     
     except Exception as e:
@@ -50,8 +51,6 @@ def restore_reference_data():
             password = 'postgres', 
             backup_file = backup_file_path
         )
-        # os.remove(backup_file_path)
-        # print('removing temp file.....')
         return 'DB restore done!!'
 
     except Exception as e:
