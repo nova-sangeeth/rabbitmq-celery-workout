90cff83abfcbae8ccf6787106823f5e0ce3b396b
nova_sangeeth <novasangeeth@outlook.com>
2021-08-30 00:09:34 +0530

    converted the code to a proper method to be used again.

--- db_utils/json _to_insert.py
+++ db_utils/json _to_insert.py
@@ -1,28 +1,57 @@
 import json
 
-TABLE_NAME = "tab"
+# TABLE_NAME = "tab"
 
-sqlstatement = ''
+# sqlstatement = ''
 with open ('data.json','r') as f:
     jsondata = json.loads(f.read())
 
-for json in jsondata:
-    keylist = "("
-    valuelist = "("
-    firstPair = True
-    for key, value in json.items():
-        if not firstPair:
-            keylist += ", "
-            valuelist += ", "
-        firstPair = False
-        keylist += key
-        if type(value) in (str, unicode):
-            valuelist += "'" + value + "'"
-        else:
-            valuelist += str(value)
-    keylist += ")"
-    valuelist += ")"
+# for json in jsondata:
+#     keylist = "("
+#     valuelist = "("
+#     firstPair = True
+#     for key, value in json.items():
+#         if not firstPair:
+#             keylist += ", "
+#             valuelist += ", "
+#         firstPair = False
+#         keylist += key
+#         if type(value) in (str, unicode):
+#             valuelist += "'" + value + "'"
+#         else:
+#             valuelist += str(value)
+#     keylist += ")"
+#     valuelist += ")"
 
-    sqlstatement += "INSERT INTO " + TABLE_NAME + " " + keylist + " VALUES " + valuelist + "\n"
+#     sqlstatement += "INSERT INTO " + TABLE_NAME + " " + keylist + " VALUES " + valuelist + "\n"
 
-print(sqlstatement)
+# print(sqlstatement)
+
+
+
+def json_to_inserts(table_name, jsondata):
+    #create a empty string.
+
+    sqlstatement = ""
+
+    for json in jsondata:
+        keylist = "("
+        valuelist = ")"
+        for key, value in json.items():
+            if not firstPair:
+                keylist += ", "
+                valuelist += ", "
+            firstPair = False
+            keylist += key
+            if type(value) in (str, unicode):
+                valuelist += "'" + value + "'"
+            else:
+                valuelist += str(value)
+
+        keylist += ")"
+        valuelist += ")"
+        print(f"column_name: {keylist}, values: {valuelist}")
+
+        sqlstatement += "INSERT INTO " + table_name + " " + keylist + " VALUES " + valuelist + "\n"
+
+
