3b574ae7e28df407d8cea77f48be4dedf1b64994
nova_sangeeth <novasangeeth@outlook.com>
2021-09-13 23:01:04 +0530

    changes to json to insert.

--- db_utils/json _to_insert.py
+++ db_utils/json _to_insert.py
@@ -2,31 +2,8 @@ import json
 
 # TABLE_NAME = "tab"
 
-# sqlstatement = ''
 with open ('data.json','r') as f:
     jsondata = json.loads(f.read())
-
-# for json in jsondata:
-#     keylist = "("
-#     valuelist = "("
-#     firstPair = True
-#     for key, value in json.items():
-#         if not firstPair:
-#             keylist += ", "
-#             valuelist += ", "
-#         firstPair = False
-#         keylist += key
-#         if type(value) in (str, unicode):
-#             valuelist += "'" + value + "'"
-#         else:
-#             valuelist += str(value)
-#     keylist += ")"
-#     valuelist += ")"
-
-#     sqlstatement += "INSERT INTO " + TABLE_NAME + " " + keylist + " VALUES " + valuelist + "\n"
-
-# print(sqlstatement)
-
 
 
 def json_to_inserts(table_name, jsondata):
@@ -53,5 +30,7 @@ def json_to_inserts(table_name, jsondata):
         print(f"column_name: {keylist}, values: {valuelist}")
 
         sqlstatement += "INSERT INTO " + table_name + " " + keylist + " VALUES " + valuelist + "\n"
+
+    return sqlstatement
 
 
