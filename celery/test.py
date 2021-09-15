37e5c9cc5016a97dfe3c3196f513dae4b8541cc5
nova_sangeeth <novasangeeth@outlook.com>
2021-09-16 00:08:08 +0530

    Changes to the celery worker files.

--- celery/test.py
+++ celery/test.py
@@ -5,13 +5,13 @@ logger = logging.getLogger(__name__)
 
 
 def testing():
-    try: 
-        logger.info('testing inside 1')
+    try:
+        logger.info("testing inside 1")
         route = celery_app.amqp.routes[0].route_for_task("worker.test_celery")
         task_1 = celery_app.send_task("worker.test_celery_2")
         logger.debug(task_1)
     except:
-        logger.info('testing inside failed')
+        logger.info("testing inside failed")
     return None
 
 
--- celery/worker.py
+++ celery/worker.py
@@ -5,7 +5,7 @@ import logging
 logger = logging.getLogger(__name__)
 celery_app = Celery("worker", broker="amqp://guest@localhost//")
 celery_app.conf.task_always_eager = True
-celery_app.conf.timezone = 'UTC'
+celery_app.conf.timezone = "UTC"
 
 
 celery_app.conf.task_routes = {
@@ -14,13 +14,12 @@ celery_app.conf.task_routes = {
 }
 
 
-
 @celery_app.task()
 def test_celery():
     logger.info("test task return")
     return f"test task return"
 
-    
+
 @celery_app.task()
 def test_celery_2():
     logger.info("test task return--------------------")
